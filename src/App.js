ebf6256b1261664d524c809103bc5a5a96f6111f
skurgan <63856344+skurgan@users.noreply.github.com>
2021-07-12 14:42:26 +0200

    broken Home component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header/Header.js';
-import Crypto from './Assets/crypto.jpg';
-import NFT from './Assets/nft.jpeg';
+import Home from './Home/Home.js';
 
 function App() {
   return (
       <div class="bg">
         <Header/>
-
-        <div class="Home">
-          <div class="nftImage">
-            <img src={NFT} class="nftPicture" onClick={()=> {console.log('nft clicked')}}/>
-            <div class="nftText">NFT MARKETPLACE</div>
-            <div class="nftHidden">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-              molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-              numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-              optio, eaque rerum! Provident similique accusantium nem</div>
-          </div>
-          <div class="cryptoImage">
-            <img src={Crypto} class="cryptoPicture"/>
-            <div class="cryptoText">CRYPTO BUNDLES</div>
-            <div class="cryptoHidden">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-              molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-              numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-              optio, eaque rerum! Provident similique accusantium nem</div>
-          </div>
-        </div>
-
+        <Home/>
       </div>
   );
 }
---
+++ src/Home/Home.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import NFT from '../Assets/nft.jpeg';
+import Crypto from '../Assets/crypto.jpg';
+import './Home.css';
+
+function Home()  {
+    return (
+        <div class="home">
+            <div class="nftImage">
+                <img src={NFT} class="nftPicture" onClick={()=> {console.log('nft clicked')}}/>
+                <div class="nftText">NFT MARKETPLACE</div>
+                <div class="nftHidden">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                optio, eaque rerum! Provident similique accusantium nem</div>
+            </div>
+            <div class="cryptoImage">
+                <img src={Crypto} class="cryptoPicture"/>
+                <div class="cryptoText">CRYPTO BUNDLES</div>
+                <div class="cryptoHidden">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                optio, eaque rerum! Provident similique accusantium nem</div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
