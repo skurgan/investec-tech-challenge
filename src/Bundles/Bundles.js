5c5e5717fd02d71c12ee890a3941dbc30e54c464
skurgan <63856344+skurgan@users.noreply.github.com>
2021-07-13 13:55:04 +0200

    done v2

--- src/Bundles/Bundles.js
+++ src/Bundles/Bundles.js
@@ -3,6 +3,10 @@ import './Bundles.css';
 
 function Bundles () {
     const [text, setText] = useState('default');
+    const [invest, setInvest] = useState(false);
+    const [investValue, setInvestValue] = useState(0);
+    const [lowRisk, setLowRisk] = useState(null);
+
     const def = "Crypto Bundles are groups of cryptocurrencies with similar risk levels. Here at Investec we have made 2 bundle groups namely high and low risk. These bundles will be made up of the coins in their respective risk category. Each bundle will comprise different percentages of the coins which will total the initial investment allocated to it.";
     const low = "A low risk investment is an investment in which there is thought to be just a small chance of losing some or all of your money. Typically, a 'low risk investment' has a low amount of upside."
      + " This bundle consists of: " + "Stable coin: 15%"
@@ -16,7 +20,27 @@ function Bundles () {
     + " , " + "Zcash: 30%"
     + " , " + "Stellar: 18%"
     + " , " + "Dogecoin: 6%";
+
+    const onInvestHandler = (isInvest) => {
+        setLowRisk(isInvest);
+        setInvest(true);
+    }
+
     return(
+        <>
+        {/* pop up */}
+        <div class={invest ? "back" : "backHidden"} onClick={()=>setInvest(false)}/>
+            <div class={invest ? "dets" : "backHidden"}>
+                <div class="investArea">
+                    <div class="exitInvest"><span onClick={()=>setInvest(false)}>close</span></div>
+                    <h2>{lowRisk ? "Low Risk Bundle" : "High Risk Bundle"}</h2>
+                    <div>You are about to invest in a bundle of cryptocurrencies and will be assigned the respective
+                        cryptocurrencies in the ratio specified (click 'more info')</div>
+                    <div><input placeholder="Enter investment amount (ZAR)" onChange={(event)=>setInvestValue(event.target.value)}/></div>
+                    <button onClick={()=>alert('You have successfully purchased a Crypto Bundle!')}>CONFIRM INVESTMENT</button>
+                </div>
+            </div>
+        {/* pop up */}
         <div class="body">
             <div class="bundles">
                 <div class="lowRisk">
@@ -24,7 +48,7 @@ function Bundles () {
                     <div class="LRST">Bitcoin, Ethereum, Stable Coin, USDC, Litecoin
                         <div class="LRB">
                             <button onClick={()=>{setText('LR')}}>MORE INFO</button>
-                            <button>INVEST</button>
+                            <button onClick={()=>onInvestHandler(true)}>INVEST</button>
                         </div>
                     </div>
                 </div>
@@ -33,7 +57,7 @@ function Bundles () {
                     <div class="HRST">Ripple, Monero, Zcash, Stellar, Dogecoin
                     <div class="HRB">
                             <button onClick={()=>{setText('HR')}}>MORE INFO</button>
-                            <button>INVEST</button>
+                            <button onClick={()=>onInvestHandler(false)}>INVEST</button>
                     </div>
                     </div>
                 </div>
@@ -42,6 +66,7 @@ function Bundles () {
                   {text == 'default' ? def : text == 'LR' ? low : text == 'HR' ? high : def}  
             </div>
         </div>
+        </>
     );
 }
 
